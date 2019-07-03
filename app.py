303ee2bb2f52908abb5649678a60c98428fdfc9d
Debparno <41444610+Debparno@users.noreply.github.com>
2019-07-04 00:33:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from keras.models import load_model
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 import pandas as pd
+import pickle
 
 app = Flask(__name__)
 
@@ -18,16 +19,19 @@ def process():
     #lastName = request.form['lastName']
     best_model =  load_model('BalanceNet1.h5')
     #data2 = pd.read_csv('train.csv')
-    text = request.form['firstName']
-    x = text.split(' ')
-    y = [int(k) for k in x]
+    x = request.form['firstName']
+    text = ['' for _ in range(5)]
+    text[0] = x
+    with open('tokenizer.pickle', 'rb') as handle:
+        tokenizer = pickle.load(handle)
+    sequences_test = tokenizer.texts_to_sequences(text)
     data_int_t = pad_sequences([y, [], [], [], []], padding='pre', maxlen=(MAX_SEQUENCE_LENGTH-5))
     data_test = pad_sequences(data_int_t, padding='post', maxlen=(MAX_SEQUENCE_LENGTH))
     y_prob = best_model.predict(data_test)
     
     #output = firstName + lastName
     if (firstName):
-        return jsonify({'output':'Neutral: ' + str(y_prob[0][0]) + 'Happiness: ' + str(y_prob[0][1]) +'Sadness: ' + str(y_prob[0][0]) + 'Hatred: ' + str(y_prob[0][0]) + 'Anger: ' + str(y_prob[0][0])})
+        return jsonify({'output':'Neutral: ' + str(y_prob[0][0]) + '  Happiness: ' + str(y_prob[0][1]) +'  Sadness: ' + str(y_prob[0][0]) + '  Hatred: ' + str(y_prob[0][0]) + '  Anger: ' + str(y_prob[0][0])})
     return jsonify({'error' : 'Missing data!'})
 
     
