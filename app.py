ac8a212764e6c7fa1ab2b615333c72bbe3b96ec4
Debparno <41444610+Debparno@users.noreply.github.com>
2019-07-03 12:16:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
-# -*- coding: utf-8 -*-
-"""
-    Calculator
-    ~~~~~~~~~~~~~~
 
-    A simple Calculator made by Flask and jQuery.
+from flask import Flask, request, render_template
 
-    :copyright: (c) 2015 by Grey li.
-    :license: MIT, see LICENSE for more details.
+app = Flask(__name__)
+
+@app.route('/', methods = ['POST'])
+def my_form_post():
+    text = request.form['u']
+    processed_text = text.upper()
+    return processed_text
+
+@app.route('/')
+def my_form():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run()
+
+
 """
 import re
 from flask import Flask, jsonify, render_template, request
@@ -23,47 +33,21 @@ import pandas as pd
 app = Flask(__name__)
 
 
-@app.route('/_calculate')
-def calculate():
-    MAX_SEQUENCE_LENGTH = 30
-    
-    best_model =  load_model('example.h5')
-    data2 = pd.read_csv('train.csv')
-    
-    data_int_t = pad_sequences([[1, 72, 19, 38], [], [], [], []], padding='pre', maxlen=(MAX_SEQUENCE_LENGTH-5))
-    data_test = pad_sequences(data_int_t, padding='post', maxlen=(MAX_SEQUENCE_LENGTH))
-    #y_prob = best_model.predict(data_test)
-    
-    return str(data2['id'][0])
+@app.route('/', methods = ['POST'])
+def my_form_post():
+    text = request.form['u']
+    processed_text = text.upper()
+    return processed_text
 
-    """
-    a = request.args.get('number1', '0')
-    operator = request.args.get('operator', '+')
-    b = request.args.get('number2', '0')
-    m = re.match('-?\d+', a)
-    n = re.match('-?\d+', b)
-    if m is None or n is None or operator not in '+-*/':
-        return jsonify(result='I Catch a BUG!')
-    if operator == '/':
-        result = y_prob[0][0]
-    else:
-        result = y_prob[0][0]
-    return jsonify(result = eval(str(y_prob[0][0]) + "+" + "0.0")) 
-    """
-
-x = 3
-y = 4
+#x = 3
+#y = 4
 #best_model =  load_model('BalanceNet.h5') 
 @app.route('/')
 def index():
-    #data = pd.read_csv('train.csv')
-    best_model =  load_model('BalanceNet.h5')
-    data_int_t = pad_sequences([[1, 72, 19, 38], [], [], [], []], padding='pre', maxlen=(30-5))
-    data_test = pad_sequences(data_int_t, padding='post', maxlen=(30))
-    y_prob = best_model.predict(data_test)
-        #print (y_prob[0][0])
-    return str(y_prob[0][0])
+    return render_template('index.html')
+    
 
 
 if __name__ == '__main__':
     app.run()
+"""
